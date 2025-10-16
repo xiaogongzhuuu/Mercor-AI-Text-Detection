ce50c204b224cbde4ad0969ca7243b7d8fddb2f2
xiaogongzhuuu <caosiyuan123456789@163.com>
2025-10-16 11:18:11 +0800

    structure

--- mercor-ai-text-detection.py
+++ mercor-ai-text-detection.py
@@ -10,10 +10,26 @@ from sklearn.isotonic import IsotonicRegression
 import lightgbm as lgb
 import xgboost as xgb
 from catboost import CatBoostClassifier
-import warnings
-warnings.filterwarnings('ignore')
-print("所有库导入完成!")
 
-train_df = pd.read_csv('/kaggle/input/mercor-ai-detection/train.csv')
-test_df = pd.read_csv('/kaggle/input/mercor-ai-detection/test.csv')
 
+def Feature_engineering():
+
+
+def embedding():
+
+
+def modeling_functions()
+
+
+def explain_prediction():
+
+
+
+def main():
+    train_df = pd.read_csv('/kaggle/input/mercor-ai-detection/train.csv')
+    test_df = pd.read_csv('/kaggle/input/mercor-ai-detection/test.csv')
+    
+
+
+if __name__ == "__main__":
+    submission=main()
